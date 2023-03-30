dc068bc8cf13d668bd0630f66d775d2c84b2b880
kztera <nguyenkhoa376@duck.com>
2023-03-30 15:24:50 +0700

    handle tab in textarea

--- script.js
+++ script.js
@@ -29,3 +29,24 @@ function addDetailTag() {
         document.getElementById('textarea').value += "\r\n\r\n<details>\r\n<summary>Click to expand</summary>\r\n\r\n</details>";
     }
 }
+
+$("textarea").keydown(function(e) {
+    if(e.keyCode === 9) { // tab was pressed
+        // get caret position/selection
+        var start = this.selectionStart;
+            end = this.selectionEnd;
+
+        var $this = $(this);
+
+        // set textarea value to: text before caret + tab + text after caret
+        $this.val($this.val().substring(0, start)
+                    + "\t"
+                    + $this.val().substring(end));
+
+        // put caret at right position again
+        this.selectionStart = this.selectionEnd = start + 1;
+
+        // prevent the focus lose
+        return false;
+    }
+});
